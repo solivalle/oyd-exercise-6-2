e8bf9358943e469672978b4683fa99315da71d2a
solivalle <127651078+solivalle@users.noreply.github.com>
2026-05-30 18:21:48 -0600

    Task 4 - Target Group

--- main.tf
+++ main.tf
@@ -19,3 +19,25 @@ resource "aws_security_group" "alb" {
   }
 }
 
+# ── Target group ──────────────────────────────────────────────────────────────
+resource "aws_lb_target_group" "this" {
+  name        = "${var.environment}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.main.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    interval            = 30
+  }
+
+  tags = {
+    Name = "${var.environment}-tg"
+  }
+}
+
